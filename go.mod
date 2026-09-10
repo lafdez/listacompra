6d0a6b53191682775e97f872bb0bf20f895ad43e
Luis Ángel Fernández Fernández <la@zzsoft.es>
2026-09-10 12:24:50 +0200

    Arrancando el repositorio

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lafdez/listacompra
+
+go 1.27.0
---
+++ listacompra.go
@@ -0,0 +1,10 @@
+package listacompra
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Aplicación web para la lista de la compra")
+	fmt.Printf("%s", "Hola")
+}
